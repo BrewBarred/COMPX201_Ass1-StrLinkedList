55504738a04388ba07d3cd32f85054da6fb6740e
Eli <elayjahh7@hotmail.com>
2024-03-09 23:24:06 +1300

    Finished StrLinkedList and Node classes

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing StrLinkedList");
+        System.out.println();
+
+        System.out.println("Creating an instance of the StrLinkedList...");
+        StrLinkedList strList = new StrLinkedList();
+        System.out.println("List is empty: " + strList.isEmpty() );
+        System.out.println("List length is: " + strList.getLength());
+        System.out.println();
+
+        System.out.println("Adding values A,B,C to the StrLinkedList ...");
+        strList.add("A");
+        strList.add("B");
+        strList.add("C");
+        strList.print();
+        System.out.println("List is empty: " + strList.isEmpty());
+        System.out.println("List has value A: " + strList.hasValue("A"));
+        System.out.println("List has value F: " + strList.hasValue("F"));
+        System.out.println("List length is: " + strList.getLength());
+        System.out.println();
+
+        strList.remove("B");
+        System.out.println();
+
+        strList.remove("A");
+        System.out.println();
+
+        strList.remove("C");
+        System.out.println();
+
+        System.out.println("List length is: " + strList.getLength());
+        System.out.println();
+
+        System.out.println("Adding values A, B, C, B, D to the StrLinkedList ...");
+        strList.add(new String[] {"A", "B", "C", "B", "D"});
+        strList.print();
+        System.out.println();
+
+        strList.remove("B");
+        System.out.println();
+
+        System.out.println("Retrieving value at position 0...");
+        System.out.println("The value at position 0: " + strList.getValueAt(0));
+        System.out.println();
+
+        System.out.println("Retrieving value at position 3...");
+        System.out.println("The value at position 3: " + strList.getValueAt(3));
+
+    } // end main
+
+} // end class
---
+++ src/StrLinkedList.java
@@ -0,0 +1,245 @@
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
+
+public class StrLinkedList {
+    public static class Node{
+
+        // The string being stored in this node
+        private String value;
+        // The node that this node is pointing to
+        private Node next;
+
+        /**
+         * Constructs a new node that stores a string and points to another null
+         */
+        public Node(String str){
+            value = str;
+            next = null;
+
+        } // end constructor
+
+        /**
+         * Overrides the default toString() method to return the details of this node
+         * @return An informative string containing the details of this node
+         */
+        @Override
+        public String toString(){
+            return "Node value: " + value;
+
+        } // end string
+
+    } // end class
+
+    // Logger object for proper error logging
+    private static final Logger logger = Logger.getLogger(StrLinkedList.class.getName());
+    // The first node in the list
+    Node head;
+
+    /**
+     * Constructs a new linked list to store string values
+     */
+    public StrLinkedList(){
+        head = null;
+
+    } // end constructor
+
+    /**
+     * Checks if this list is currently empty or not
+     * @return A boolean value that is true if the list is empty, else returns false
+     */
+    public boolean isEmpty(){
+        // if the head is null, this list must be empty
+        return head == null;
+
+    } // end boolean
+
+    /**
+     * Gets the length of this list
+     * @return An integer value denoting the length of this list (i.e., the number of nodes/elements)
+     */
+    public int getLength(){
+        Node current = head;
+        int length = 0;
+
+        // iterates through the list and counts each node/element to determine the length of this list
+        while (current != null){
+            length++;
+            current = current.next;
+
+        } // end while
+
+        return length;
+
+    } // end int
+
+    /**
+     * Checks if this list contains the passed string value
+     * @param string The string to search for in this list
+     * @return A boolean value that is true if the linked list contains
+     * a node whose value is the same the passed string value
+     */
+    public boolean hasValue(String string){
+        Node current = head;
+
+        // iterates through each list value returning true if a match is found
+        while (current != null){
+
+            if (current.value.equals(string))
+                return true;
+            current = current.next;
+
+        } // end while
+
+        return false;
+
+    } // end boolean
+
+    /**
+     * Gets the value at the passed index of this list
+     * @param index The index of the string value to return
+     * @return The string value at the passed index of this list
+     */
+    public String getValueAt(int index){
+        try {
+            // check for invalid values
+            if (getLength() == 0)
+                throw new IndexOutOfBoundsException("Unable to return ");
+            if (index < 0 || index > getLength() - 1)
+                throw new IndexOutOfBoundsException("Failed to return value at index " + index + ", index was out of bounds!\n" +
+                        " Please ensure passed index value is between 0 and " + (getLength() - 1));
+
+            int i = 0;
+            String string = "";
+            Node current = head;
+
+            // iterates through each node until the desired index is reached, returning that nodes value
+            while (current != null) {
+
+                if (i == index)
+                    return current.value;
+                current = current.next;
+                i++;
+
+            } // end while
+
+            return string;
+
+        } catch (Exception error) {
+            logger.severe(error.getMessage());
+            return null;
+
+        } // end try
+
+    } // end string
+
+    /**
+     * Creates a new node with the passed string value and adds it to the head of this list
+     * @param string The value to add to the head of this list
+     */
+    public void add(String string){
+        Node newNode = new Node(string);
+
+        // if the head is not null, the new node should be pointing to it
+        if (head != null)
+            newNode.next = head;
+        head = newNode;
+
+        System.out.println("Successfully added \"" + string + "\" to this list");
+
+    } // end void
+
+    /**
+     * Creates a new node for each element in the passed string array and adds them to the head of this list
+     * @Note: The string values will be added in the order that they appear in the array, starting from index 0
+     * @param stringArray An array containing the values to add to the head of this list
+     */
+    public void add(String[] stringArray){
+        for (String string : stringArray) {
+            add(string);
+
+        } // end for
+
+    } // end void
+
+    /**
+     * Removes the node containing the first found occurrence of the passed string value from this list
+     * @param string The string value to remove the list
+     */
+    public void remove(String string){
+        try {
+            if (isEmpty())
+                throw new IllegalArgumentException("Failed to remove value \"" + string + "\", list is empty!");
+            if (!hasValue(string))
+                throw new NoSuchElementException("Failed to remove value \"" + string + "\", value not found!");
+
+            System.out.println("Removing value \"" + string + "\" from the StrLinkedList ...");
+
+            // cuts head off if value is contained in the head
+            if (head.value.equals(string)) {
+                head = head.next;
+                print();
+                return;
+
+            } // end if
+
+            Node previous = head;
+            Node current = head.next;
+
+            // iterates through the list until the value is found, then removes it
+            while(current != null){
+
+                if(current.value.equals(string)) {
+                    previous.next = current.next;
+                    this.print();
+                    return;
+
+                } // end if
+
+                // moves both pointers to the next nodes
+                previous = current;
+                current = current.next;
+
+            } // end while
+
+        }  catch (Exception error) {
+            logger.severe(error.getMessage());
+
+        } // end try
+
+    } // end void
+
+    /**
+     * Constructs a detailed string of each node in this list
+     * @return A string value containing the nodes value and the node that it is pointing to
+     */
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (!isEmpty()) {
+            Node current = head;
+
+            // for each node in the list, print its value, followed by the node it is pointing to
+            while (current != null) {
+                stringBuilder.append(current.value).append(" -> ");
+                current = current.next;
+
+            } // end while
+
+        } // end if
+
+        stringBuilder.append("null");
+        return stringBuilder.toString();
+
+    } // end string
+
+    /**
+     * Prints the string value of each node, followed by a pointer to
+     * it's next node (->) in the order that they appear in the list
+     */
+    public void print(){
+        System.out.println(this);
+
+    } // end void
+
+} // end class
